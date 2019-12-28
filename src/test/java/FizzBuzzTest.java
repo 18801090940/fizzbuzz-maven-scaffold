0b4ca9f55dd84f6d832fc067a822347a251484e7
wangwei <710207185@qq.com>
2019-12-28 13:12:27 +0800

    refractor Java_王威_技术测试

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,22 +4,15 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
     @Test
-    public void should_return_value_of_number(){
-        assertThat(FizzBuzz.of(1)).isEqualTo("1");
+    public void test_all_case()
+    {
+        assert_result(1, "1");
+        assert_result(3, "Fizz");
+        assert_result(5, "Buzz");
+        assert_result(15, "FizzBuzz");
     }
-
-    @Test
-    public void should_return_Fizz_given_number_divided_by_three(){
-        assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");
-    }
-
-    @Test
-    public void should_return_Buzz_given_number_divided_by_five(){
-        assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
-    }
-
-    @Test
-    public void should_return_FizzBuzz_given_number_divided_by_three_and_five(){
-        assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
+    public void assert_result(int number, String result)
+    {
+        assertThat(FizzBuzz.of(number)).isEqualTo(result);
     }
 }
